0cb6d438a40aeac138ab33f84c5daa348d9c0969
Vasily Yakovenko <bitnikvasya@mail.ru>
2020-04-24 16:34:39 +0300

    Updates

---
+++ precache-manifest.2fd803d74db26aa4496929b91c5339e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff817fca9861cc76432cdeb30c8a997",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "f9ea942215e40e441f81",
+    "url": "/todo/static/css/main.fce1a370.chunk.css"
+  },
+  {
+    "revision": "8794c74e404ca9f65638",
+    "url": "/todo/static/js/2.74e00c99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.74e00c99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ea942215e40e441f81",
+    "url": "/todo/static/js/main.1f49c822.chunk.js"
+  },
+  {
+    "revision": "5131de2ed6b872720718",
+    "url": "/todo/static/js/runtime-main.7c477364.js"
+  }
+]);
